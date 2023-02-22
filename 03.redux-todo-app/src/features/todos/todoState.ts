8f63f10fe7ee322c905c54b0da4bd11307c67029
IYEMON018 <iyemon018@gmail.com>
2023-02-23 00:01:06 +0900

    wip State の定義まで実装した。

--- 03.redux-todo-app/src/features/todos/todoState.ts
+++ 03.redux-todo-app/src/features/todos/todoState.ts
@@ -1,1 +1,22 @@
 // 状態を定義します。
+import { Todo } from "../../common/todo.type";
+
+/**
+ * アプリケーションの保持する Todo の配列です。s
+ * @returns Todo リストの情報をすべて返します。
+ */
+export const state = (): Todo[] => {
+    return [
+        {
+            id: 1,
+            title: "テスト1",
+            content: "テスト1の内容",
+            isCompleted: false,
+        }, {
+            id: 2,
+            title: "テスト2",
+            content: "テスト2の内容",
+            isCompleted: false,
+        }
+    ]
+}
