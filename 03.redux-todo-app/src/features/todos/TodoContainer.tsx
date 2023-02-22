8d8ad71c9bff500b69557b2c312195ac309c8b53
IYEMON018 <iyemon018@gmail.com>
2023-02-23 00:16:56 +0900

    wip コミット漏れ

--- 03.redux-todo-app/src/features/todos/TodoContainer.tsx
+++ 03.redux-todo-app/src/features/todos/TodoContainer.tsx
@@ -1,1 +1,14 @@
 // TODO アプリのロジック部分を実装します。
+
+import { useSelector } from "react-redux"
+import { RootState } from "../../common/rootState.type";
+
+export const TodoContainer = () => {
+    const todos = useSelector((state: RootState) => state.todos);
+
+    const args = {
+        todos,
+    };
+
+    return <TodoPresenter {...args} />
+}
--- 03.redux-todo-app/src/features/todos/todoAction.ts
+++ 03.redux-todo-app/src/features/todos/todoAction.ts
@@ -1,1 +1,39 @@
 // Redux で言うところの ActionCreator の役割を果たす。
+
+import { Todo } from "../../common/todo.type";
+
+/**
+ * Todo を新規追加するための Action　です。
+ * @param newTodo 新規追加対象の Todo 情報を設定します。
+ * @returns Todo 新規追加アクションを返します。
+ */
+export const addTodoAction = (newTodo: Todo) => {
+    return {
+        type: "ADD",
+        payload: newTodo,
+    };
+};
+
+/**
+ * 既存の Todo の完了状態を変更するための Action です。
+ * @param id 完了状態を切り替える対象の Todo 識別子を設定します。
+ * @returns Todo 完了状態変更アクションを返します。
+ */
+export const toggleCompleteAction = (id: number) => {
+    return {
+        type: "TOGGLE_COMPLETE",
+        payload: id,
+    };
+};
+
+/**
+ * 既存の Todo を削除するための Action です。
+ * @param id 削除対象の Todo 識別子を設定します。
+ * @returns Todo 削除アクションを返します。
+ */
+export const removeTodoAction = (id: number) => {
+    return {
+        type: "REMOVE",
+        payload: id,
+    };
+};
