08e4ad99abc415d8c9b3a61853d57e4df5b02bad
IYEMON018 <iyemon018@gmail.com>
2023-02-26 01:46:51 +0900

    Reducer を関数に変えてみた

--- 03.redux-todo-app/src/common/todo.type.ts
+++ 03.redux-todo-app/src/common/todo.type.ts
@@ -1,8 +1,35 @@
 // 当アプリで出てくる TODO のタイプを定義する。
 
+/**
+ * Todo タスクの持つ情報を定義する型です。
+ */
 export type Todo = {
+    /**
+     * タスクの識別子です。
+     */
     id: number;
+    /**
+     * タスク内容を簡略に表したタイトルです。
+     */
     title: string;
+    /**
+     * タスク内容を詳細に表したコンテンツです。
+     */
     content: string;
+
+    /**
+     * このタスクが完了したかどうかです。
+     * true: 完了済み, false: 未完了
+     */
     isCompleted: boolean;
+}
+
+/**
+ * アプリケーションで管理するすべての Todo の状態を定義する型です。
+ */
+export type TodoStateType = {
+    /**
+     * 当アプリケーションのすべての Todo 情報です。s
+     */
+    todos: Todo[];
 }
