d724a9302fb64a10910b2b262eacfcc518479495
IYEMON018 <iyemon018@gmail.com>
2023-03-02 00:19:05 +0900

    wip checkbox field を分けた

---
+++ 04.total-population-transition-graph-by-prefecture/src/components/CheckboxFields.tsx
@@ -0,0 +1,57 @@
+import { ChangeEvent } from "react";
+
+/**
+ * チェック状態が変更されたときのチェックボックスの状態を定義します。
+ */
+export type CheckboxChangedArgs = {
+    /**
+     * 変更されたチェックボックスのラベルの値です。
+     */
+    name: string;
+
+    /**
+     * 変更後の値です。
+     */
+    selected: boolean;
+}
+
+/**
+ * チェック状態が変更されたときに呼ばれるイベントを定義します。
+ */
+export type CheckboxChangedEvent = (value: CheckboxChangedArgs) => void;
+
+/**
+ * CheckBoxField のプロパティを定義します。
+ * @see {CheckBoxField}
+ */
+export type CheckBoxFieldProps = {
+    /**
+     * 表示用のラベルに出力する値です。
+     */
+    name: string;
+
+    /**
+     * チェック状態が変更されたときに呼ばれるイベントです。
+     */
+    onChanged: (name: string, checked: boolean) => void;
+}
+
+/**
+ * チェックボックスの入力フィールド コンポーネントです。
+ * @param param
+ * @returns チェックボックス コンポーネント
+ */
+export const CheckBoxField = (props: CheckBoxFieldProps) => {
+    function handleChangeda(e: ChangeEvent<HTMLInputElement>): void {
+        props.onChanged(props.name, e.target.checked);
+    }
+
+    return (
+        <div>
+            <input type="checkbox"
+                defaultChecked={false}
+                onChange={(e) => handleChangeda(e)} />
+            <label>{props.name}</label>
+        </div>
+    );
+}
