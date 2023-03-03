47315196ffaffc9ed4291a1b75cf3a6e84e63fa1
IYEMON018 <iyemon018@gmail.com>
2023-03-04 00:02:40 +0900

    wip API 呼び出しまで実装した

--- 04.total-population-transition-graph-by-prefecture/src/App.tsx
+++ 04.total-population-transition-graph-by-prefecture/src/App.tsx
@@ -1,9 +1,21 @@
+import { useEffect } from 'react';
 import './App.css';
 import { AppTitle } from './components/AppTitle/AppTitle';
 import { Prefectures } from './components/Prefectures/Prefectures';
 import { TransitionGraph } from './components/TransitionGraph/TransitionGraph';
 
 function App() {
+  useEffect(()=>{
+    // cf. https://developer.mozilla.org/ja/docs/Web/API/Fetch_API/Using_Fetch
+    fetch("https://opendata.resas-portal.go.jp/api/v1/prefectures", {
+      method: "GET",
+      headers: {
+        'Content-Type': 'application/json;charset=UTF-8',
+        'X-API-KEY': '<API Token>'
+      }
+    }).then((response) => response.json()).then((data) => console.log(data));
+  }, []);
+
   return (
     <div className="App">
       <AppTitle />
