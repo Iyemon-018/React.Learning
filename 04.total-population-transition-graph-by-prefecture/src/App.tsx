075c0f99437c0c7a5abc2081b5ad8afb79b9ba0e
IYEMON018 <iyemon018@gmail.com>
2023-03-14 23:14:58 +0900

    wip 選択した都道府県のグラフが出るところまで実装した

--- 04.total-population-transition-graph-by-prefecture/src/App.tsx
+++ 04.total-population-transition-graph-by-prefecture/src/App.tsx
@@ -6,6 +6,10 @@ import { TransitionGraph } from './components/TransitionGraph/TransitionGraph';
 import { useLocation } from 'react-router-dom';
 import { Prefecture } from './types/prefecture';
 
+/**
+ * 選択可能な都道府県の情報を定義するクラスです。
+ * このクラスはチェックボックスで都道府県を表現する目的で使用します。
+ */
 class PrefectureValue implements Prefecture {
 
   constructor(code: number, name: string) {
@@ -14,10 +18,71 @@ class PrefectureValue implements Prefecture {
     this.selected = false;
   }
 
+  /**
+   * 都道府県コードを設定、または取得します。
+   */
   code: number;
-  name: string;
-  selected: boolean;
 
+  /**
+   * 都道府県の名称を設定、または取得します。
+   */
+  name: string;
+
+  /**
+   * 現在の選択状態を設定、または取得します。
+   */
+  selected: boolean;
+}
+
+/**
+ * 1年あたりの人口推移量を定義するクラスです。
+ */
+export class TotalPopulationPerYear {
+
+  constructor(year: number, value: number) {
+    this.year = year;
+    this.value = value;
+  }
+
+  /**
+   * この情報が記録されている年数を設定、または取得します。
+   */
+  year: number;
+
+  /**
+   * この年の人口推移を設定、または取得します。
+   */
+  value: number;
+}
+
+/**
+ * 都道府県の総人口を集計した情報を定義するクラスです。
+ * ここにはある1つの都道府県における5年ごとの人口推移の情報を保持します。
+ */
+export class TotalPopulationAggregate {
+
+  constructor(prefCode: number, prefName: string, perYears: TotalPopulationPerYear[]) {
+    this.prefCode = prefCode;
+    this.prefName = prefName;
+    this.perYears = perYears;
+  }
+
+  /**
+   * 都道府県コードを設定、または取得します。
+   */
+  prefCode: number;
+
+  /**
+   * 都道府県の名称を設定、または取得します。
+   */
+  prefName: string;
+
+  /**
+   * 5年ごとの人口推移を設定、または取得します。
+   * 1要素に付き1年の情報を保持します。
+   * 人口推移の情報は5年ごとに集計する想定のため、すべての年数の情報が存在するわけではありません。
+   */
+  perYears: TotalPopulationPerYear[];
 }
 
 function App() {
@@ -25,6 +90,7 @@ function App() {
   // この状態には API Token が含まれている。
   const { state } = useLocation();
   const [prefectures, setPrefectures] = useState(Array<PrefectureValue>(47));
+  const [aggregates, setAggregates] = useState(Array<TotalPopulationAggregate>());
 
   useEffect(() => {
     // cf. https://developer.mozilla.org/ja/docs/Web/API/Fetch_API/Using_Fetch
@@ -37,18 +103,65 @@ function App() {
         }
       });
       const json = await response.json();
-      const newPrefectures = json.result.map((v: { prefCode: number, prefName: string }) => new PrefectureValue(v.prefCode, v.prefName));     // とりあえずデバッグでレスポンスをログ表示している。
+      const newPrefectures = json.result
+        .map((v: {
+          prefCode: number,
+          prefName: string
+        }) => new PrefectureValue(v.prefCode, v.prefName));
+
       setPrefectures(newPrefectures);
     })()
   }, []);
+
+  /**
+   * 都道府県のチェックボックスの状態が変更されたときに呼ばれるイベントハンドラです。
+   * @param v チェック状態が変更された都道府県の情報が設定されます。
+   */
+  async function handlePrefectureChanged(v: Prefecture): Promise<void> {
+    // cf. https://opendata.resas-portal.go.jp/docs/api/v1/population/composition/perYear.html
+    // 選択された都道府県の人口構成を取得する。
+    const prefCode = v.code;
+    const response = await fetch(`https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?prefCode=${prefCode}&cityCode=-`, {
+      method: "GET",
+      headers: {
+        'Content-Type': 'application/json;charset=UTF-8',
+        'X-API-KEY': state.apiToken
+      }
+    });
+
+    const json = await response.json();
+
+    // "label": "総人口" のデータが必要になる。
+    // ここには5年ごとの人口推移が入っており、これをグラフに出力したい。
+    // 以下はグラフに出力するための形式に変換している。
+    const totalPopulationJson = json.result
+      .data
+      .find((v:
+        {
+          label: string,
+          data: {
+            year: number,
+            value: number
+          }
+        }) => v.label === "総人口");
+    const perYears = totalPopulationJson.data
+      .map((v: {
+        year: number,
+        value: number
+      }) => new TotalPopulationPerYear(v.year, v.value));
+    const aggregate = new TotalPopulationAggregate(v.code, v.name, perYears);
+    const newValue = [...aggregates, aggregate];
+
+    setAggregates(newValue);
+  }
 
   return (
     <div>
       <AppTitle />
       <Prefectures
-        onPrefectureChanged={(v) => console.log(`${v.name}[${v.code}]: ${v.selected}`)}
+        onPrefectureChanged={(v) => handlePrefectureChanged(v)}
         prefectures={prefectures} />
-      <TransitionGraph />
+      <TransitionGraph aggregates={aggregates} />
     </div>
   );
 }
--- 04.total-population-transition-graph-by-prefecture/src/components/TransitionGraph/TransitionGraph.tsx
+++ 04.total-population-transition-graph-by-prefecture/src/components/TransitionGraph/TransitionGraph.tsx
@@ -1,6 +1,54 @@
+import { useRef } from "react";
+import Highcharts from "highcharts";
+import HighchartsReact from "highcharts-react-official";
+import { TotalPopulationAggregate } from "../../App";
 
-export const TransitionGraph = () => {
+type TransitionGraphProps = HighchartsReact.Props & {
+    aggregates: TotalPopulationAggregate[];
+}
+
+export const TransitionGraph = (props: TransitionGraphProps) => {
+    const componentRef = useRef<HighchartsReact.RefObject>(null);
+    // props の総人口データが親コンポーネントから書き換えられるので useState にする必要はない。
+    // props が変更されるたびに series を書き換えることでグラフが動的に変更されるようになる。
+    const chartOptions = {
+        chart: {
+            type: "line"
+        },
+        xAxis: {
+            categories: [] as string[],
+            title: "年数",
+            tickInterval: 5,
+        },
+        series: [
+            { name: "no.1", data: [] as number[][] }
+        ],
+        plotOptions: {
+            line: {
+                dataLabels: {
+                    enabled: true,
+                    format: '{value:,.0f}',
+                },
+                enableMouseTracking: true,
+            }
+        }
+    }
+
+    chartOptions.series = props.aggregates
+        .map(aggregate => {
+            return {
+                name: aggregate.prefName,
+                data: aggregate.perYears.map(total => [total.year, total.value]),
+            }
+        });
+
     return (
-        <h3>ここにいい感じのグラフが出るよ。</h3>
+        <div>
+            <HighchartsReact
+                highcharts={Highcharts}
+                options={chartOptions}
+                ref={componentRef}
+                {...props} />
+        </div>
     );
 }
